518b12bdb12e72605e0df9c53db383d78636a3f5
gabrielwickenson <gabrielwickenson18@gmail.com>
2026-06-26 12:40:10 -0400

    Mise à jour client.js pour l'intégration React

---
+++ web/kolabor/src/api/client.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://localhost:8080/api', // URL de votre backend (adaptez si besoin)
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// Intercepteur pour ajouter le token JWT à chaque requête
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export default api;
